31e8e97a816101c1eb2c91d888d2775be171689b
Howard Tyson <howard@howardtyson.com>
2016-09-29 14:17:19 -0700

    Fleshing out tests a bit.

--- test/server.js
+++ test/server.js
@@ -1,33 +1,31 @@
 'use strict';
 
-var async = require('async');
 var bunyan = require('bunyan');
-var nock = require('nock');
 var eventbus = require('probo-eventbus');
-var request = require('request');
 var should = require('should');
 var through2 = require('through2');
 
 var lib = require('..');
 var Server = lib.Server;
 
+var TestPlugin = require('./fixtures/TestPlugin');
+var getTestBuildEvent = require('./fixtures/buildEvent');
+
 // We use a simple method to reset the world between tests by
 // using global variables.
 var stream = null;
 var server = null;
-var storage = null;
 
 describe('Server', function() {
-  const apiServerHost = 'localhost';
-  const apiServerPort = 3038;
-  const containerManagerUrl = 'http://localhost:9631';
-
-  describe('event storage', function() {
+  describe('webhook routing', function() {
     beforeEach(function(done) {
       stream = through2.obj();
       var options = {
         consumer: new eventbus.plugins.Memory.Consumer({stream}),
         log: bunyan.createLogger({name: 'reaper-tests'}),
+        plugins: {
+          test: TestPlugin,
+        },
       };
       options.log._level = Number.POSITIVE_INFINITY;
       server = new Server(options);
@@ -36,20 +34,14 @@ describe('Server', function() {
     afterEach(function(done) {
       server.stop(done);
     });
-    it('should export data', function(done) {
-      stream.write(getTestBuildEvent());
+    it('send notifications via the appropriate plugin', function(done) {
       server.on('buildReceived', function() {
-        request(`http://${apiServerHost}:${apiServerPort}/api/export-data`, function(error, response, body) {
-          should.not.exist(error);
-          response.statusCode.should.equal(200);
-          body = body.split('\n');
-          JSON.parse(body[0]).key.should.equal('build!!build 1');
-          JSON.parse(body[1]).key.should.equal('build_date!!2016-02-27T05:44:46.947Z!!build 1');
-          JSON.parse(body[2]).key.should.equal('organization_build!!organization 1!!2016-02-27T05:44:46.947Z!!build 1');
-          JSON.parse(body[3]).key.should.equal('project_branch_build!!project 1!!branch 1!!2016-02-27T05:44:46.947Z!!build 1');
-          done();
-        });
+        should.exist(server);
+        done();
       });
+      stream.write(getTestBuildEvent());
     });
   });
 });
+
+
